74126cb6bed56f6f65e5b365f342c9d6ea6d2145
lucan <lucanlc0417@gmail>
2023-07-03 11:28:52 +0800

    地区和等级的静态搭建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,6 +4,18 @@
     <Carousel />
     <!-- 首页搜索医院的表单区域（搜索框） -->
     <Search />
+    <!-- 展示医院详细信息 -->
+    <el-row gutter="20">
+      <el-col :span="20">
+        <!-- 等级子组件 -->
+        <Level />
+        <!-- 地区子组件 -->
+        <Region />
+      </el-col>
+      <el-col :span="4">
+        <h1>医院详情</h1>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -12,6 +24,10 @@
 import Carousel from "./carousel/index.vue";
 // 引入搜索框组件
 import Search from "./search/index.vue";
+// 引入医院等级组件
+import Level from "./level/index.vue";
+// 引入地区组件
+import Region from "./region/index.vue";
 </script>
 
 <style scoped></style>
---
+++ src/pages/home/region/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="region">
+    <div class="content">
+      <div class="left">地区:</div>
+      <ul>
+        <li class="active">全部</li>
+        <li v-for="item in 30" :key="item">朝阳区</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.region {
+  color: #7f7f7f;
+  margin-top: 10px;
+  .content {
+    display: flex;
+    .left {
+      margin-right: 10px;
+      width: 90px;
+    }
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+      li {
+        margin-right: 10px;
+        margin-bottom: 5px;
+        &.active {
+          color: #55a6fe;
+        }
+      }
+      li:hover{
+        cursor: pointer;
+        color: #55a6fe;
+      }
+    }
+  }
+}
+</style>
