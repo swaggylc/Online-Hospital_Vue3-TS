47233b63c53f0cc3440c05b54bf96dbc5c8eb19c
lucan <lucanlc0417@gmail.com>
2023-07-08 20:19:37 +0800

    获取订单详情数据

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,12 +1,17 @@
 // 引入二次封装的axios
 import request from "@/utils/request";
 // 引入ts类型
-import { CreateOrderResponseData } from "@/api/user/type.ts";
+import {
+  CreateOrderResponseData,
+  GetOrderInfoResponseData,
+} from "@/api/user/type.ts";
 
 // 枚举请求地址
 enum API {
   // 创建订单,获取订单号的接口
   CREATEORDER_URL = "/order/orderInfo/auth/submitOrder/",
+  // 获取订单详情的数据
+  GETORDERINFO_URL = "/order/orderInfo/auth/getOrderInfo/",
 }
 // 提交订单
 export const createOrder = (
@@ -17,4 +22,8 @@ export const createOrder = (
   return request.post<any, CreateOrderResponseData>(
     API.CREATEORDER_URL + `${hoscode}/${scheduleId}/${patientId}`
   );
+};
+// 获取订单详情的方法
+export const getOrderInfo = (id: number) => {
+  return request.get<any, GetOrderInfoResponseData>(API.GETORDERINFO_URL + id);
 };
--- src/api/user/type.ts
+++ src/api/user/type.ts
@@ -7,3 +7,37 @@ export interface ResponseData {
 export interface CreateOrderResponseData extends ResponseData {
   data: number;
 }
+// 订单详情数据的ts类型
+export interface OrderInfoData {
+  id: number;
+  createTime: string;
+  updateTime: string;
+  isDeleted: number;
+  param: {
+    orderStatusString: string;
+  };
+  userId: number;
+  outTradeNo: string;
+  hoscode: string;
+  hosname: string;
+  depcode: string;
+  depname: string;
+  title: string;
+  scheduleId: string;
+  reserveDate: string;
+  reserveTime: number;
+  amount: number;
+  patientId: number;
+  patientName: string;
+  patientPhone: string;
+  hosRecordId: string;
+  number: number;
+  fetchTime: string;
+  fetchAddress: string;
+  quitTime: string;
+  orderStatus: number;
+}
+// 获取订单详情接口返回的数据类型
+export interface GetOrderInfoResponseData extends ResponseData {
+  data: OrderInfoData;
+}
