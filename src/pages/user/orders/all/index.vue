5224867e904bad0b9996a75bc13a639b31ceef89
lucan <lucanlc0417@gmail.com>
2023-07-10 10:32:59 +0800

    完成挂号订单模块的静态搭建

--- src/pages/user/orders/all/index.vue
+++ src/pages/user/orders/all/index.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>
-        所有订单
-    </div>
+    <el-card class="box-card">
+        <template #header>
+            <div class="card-header">
+                <span>挂号订单</span>
+            </div>
+        </template>
+        <!-- 提供用户选择的下拉菜单 -->
+        <el-form :inline="true" class="demo-form-inline">
+            <el-form-item label="就诊人">
+                <el-select placeholder="请选择就诊人">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                </el-select>
+            </el-form-item>
+            <el-form-item label="订单状态">
+                <el-select placeholder="请选择订单状态">
+                    <el-option label="Zone one" value="shanghai" />
+                    <el-option label="Zone two" value="beijing" />
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <!-- 展示订单数据的表格 -->
+        <el-table height="250" style="width: 100%;margin: 20px 0;" :border="true">
+            <el-table-column prop="date" label="就诊时间" />
+            <el-table-column prop="name" label="医院" />
+            <el-table-column prop="address" label="科室" width="200" />
+            <el-table-column prop="address" label="医生" />
+            <el-table-column prop="address" label="医事服务费" />
+            <el-table-column prop="address" label="就诊人" />
+            <el-table-column prop="address" label="订单状态" />
+            <el-table-column prop="address" label="操作" />
+        </el-table>
+        <!-- 分页器 -->
+        <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[10, 20, 30, 40]"
+            layout=" jumper, prev, pager, next,->,sizes,total" :total="400" :background="true" />
+    </el-card>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+
+let pageNo = ref<number>(1)
+let pageSize = ref<number>(10)
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/pages/user/orders/index.vue
+++ src/pages/user/orders/index.vue
@@ -11,9 +11,7 @@
 import All from "./all/index.vue";
 import Detail from "./detail/index.vue";
 
-import { useRouter } from "vue-router";
 import { useRoute } from "vue-router";
-let $router = useRouter();
 let $route = useRoute();
 </script>
 
